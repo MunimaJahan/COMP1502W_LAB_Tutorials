000c21129fad6fe04b66aba4e03b28d975dc57f4
Munima <Munima@10.0.0.141>
2021-01-14 09:35:27 -0700

    is prime added

--- Review/src/PrintSomething.java
+++
@@ -1,9 +0,0 @@
-
-public class PrintSomething {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.print("This is a test");
-	}
-
-}
---
+++ Review/src/mainPrime.java
@@ -0,0 +1,19 @@
+
+public class mainPrime {
+
+	  public static void main(String[] args) {
+		    System.out.println("Hello Prime!");
+		    
+		    System.out.println(/*isPrime(2)*/);
+		    System.out.println(/*isPrime(3)*/);
+		    System.out.println(/*isPrime(4)*/);
+		    System.out.println(/*isPrime(10)*/);
+		    System.out.println(/*isPrime(11)*/);
+		    System.out.println(/*isPrime(12)*/);
+		}
+		//isPrime goes here
+		  
+		
+
+
+}
