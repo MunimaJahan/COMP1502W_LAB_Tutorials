d02fcb81b47325359964ea67744c4614c3721fab
MunimaJahan <59153345+MunimaJahan@users.noreply.github.com>
2021-01-14 13:33:55 -0700

    Update mainPrime.java

--- Review/src/mainPrime.java
+++ Review/src/mainPrime.java
@@ -7,24 +7,13 @@ public class mainPrime {
 		    
 		    System.out.println(/*isPrime(2)*/);
 		    System.out.println(/*isPrime(3)*/);
-		    System.out.println(isPrime(4));
-		    System.out.println(isPrime(10));
+		    System.out.println(/*isPrime(4)*/);
+		    System.out.println(/*isPrime(10)*/);
 		    System.out.println(/*isPrime(11)*/);
-		    System.out.println(isPrime(13));
+		    //System.out.println(isPrime(13));
 		}
 		//isPrime goes here
-		public static boolean isPrime(int a) {
-			//boolean prime=true;
-			for(int i=2; i<=a/2;i++) {
-				if(a%i==0) {
-					//prime=false;
-					//break;
-					return false;
-				}
-				
-			}
-			return true;
-		}
+		
 		
 
 
