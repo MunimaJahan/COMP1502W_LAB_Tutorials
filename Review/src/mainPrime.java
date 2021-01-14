7aa800b6eacec48ba719f91f3e192142d3519c0d
Munima <Munima@10.0.0.141>
2021-01-14 14:56:54 -0700

    isPrime added

--- Review/src/mainPrime.java
+++ Review/src/mainPrime.java
@@ -5,15 +5,26 @@ public class mainPrime {
 	public static void main(String[] args) {
 		    System.out.println("Hello Prime!");
 		    
-		    System.out.println(/*isPrime(2)*/);
+		    System.out.println(isPrime(2));
 		    System.out.println(/*isPrime(3)*/);
-		    System.out.println(isPrime(4));
-		    System.out.println(/*isPrime(10)*/);
+		    System.out.println(isPrime(15));
+		    System.out.println(isPrime(10));
 		    System.out.println(/*isPrime(11)*/);
 		    System.out.println(isPrime(13));
 		}
 		//isPrime goes here
-	
+	public static boolean isPrime(int a) {
+		boolean prime=true;
+		for(int i=2;i<=a/2;i++) {
+			if(a%i==0) {
+				prime=false;
+				break;
+				//return false;
+			}
+			
+		}
+		return prime;
+	}
 		
 
 
